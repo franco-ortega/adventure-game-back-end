4b6dbd72d1feb8a3be29857c6e478ac7b0cf0c2d
Franco Ortega <francopdx@gmail.com>
2021-02-22 18:02:46 -0800

    add POST test for User model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,17 @@ const app = require('../lib/app');
 
 describe('01-adventure-game routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+
+  it('sample test', () => {
+
+    expect(true).toEqual(true);
+
   });
 });
---
+++ __tests__/users.test.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('creates a new user via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: 'Santiago',
+        email: 'santiago@test.com'
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Santiago',
+      email: 'santiago@test.com'
+    });
+  });
+});
