55dbe7d12d789650f5212bbed6ac118092c4ea67
Franco Ortega <francopdx@gmail.com>
2021-02-24 15:41:27 -0800

    update create method in UserService; update users POST test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -14,12 +14,14 @@ describe('user routes', () => {
 
   it('creates a new user via POST', async() => {
     const res = await request(app)
-      .post('/api/v1/users')
+      .post('/api/v1/auth/signup')
       .send({
         username: 'Santiago',
         email: 'santiago@test.com',
         password: 'password'
       });
+
+    console.log(res.body);
 
     expect(res.body).toEqual({
       id: expect.any(String),
