17e52ef05c39e362cd37852c20faabeb644be9fb
Franco Ortega <francopdx@gmail.com>
2021-02-26 23:14:07 -0800

    add GET test for Character model to get all characters

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
+const Character = require('../lib/models/Character');
 
 describe('character routes', () => {
   beforeEach(() => {
@@ -39,5 +40,43 @@ describe('character routes', () => {
       gender: 'male',
       userId: user.id
     });
+  });
+
+  it('get all characters via GET', async() => {
+    // const user = await UserService.create({
+    //   username: 'Anna',
+    //   email: 'Anna@test.com',
+    //   password: 'password'
+    // });
+
+    const characters = await Promise.all([
+      {
+        characterName: 'Tarven',
+        species: 'Elf',
+        hitPoints: 30,
+        gender: 'male',
+        userId: Character.user_id
+      },
+      {
+        characterName: 'Sheila',
+        species: 'Human',
+        hitPoints: 30,
+        gender: 'female',
+        userId: Character.user_id
+      },
+      {
+        characterName: 'Helga',
+        species: 'Dwarf',
+        hitPoints: 30,
+        gender: 'female',
+        userId: Character.user_id
+      }
+    ]).map(character => Character.insert(character));
+
+    const res = await request(app)
+      .get('/api/v1/characters');
+
+    expect(res.body).toEqual(expect.arrayContaining(characters));
+    expect(res.body).toHaveLength(characters.length);
   });
 });
